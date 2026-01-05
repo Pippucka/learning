4f036e5a169506b072dbd98ed1109574f1e9ce9a
Pippucka <140398690+Pippucka@users.noreply.github.com>
2026-01-05 17:52:33 +0300

    massive

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// document.write("hello");
+console.log("Привет братва");
+
+var num = 15;
+var stroka = "punp"
+
+if(num == 15)
+{
+    console.log("num = " + num);
+    console.log("Ok");
+}
+else
+{
+    
+    console.error("Error!")
+}
+
+switch(stroka) 
+{
+    case "swat":
+        console.log("Ты допизделся дружок")
+        break;
+    case "dox":
+        console.log("Запускаю глаз боба")
+        break;
+}
+
+var arr = [5,15,20,25,30];
+console.log("массов:" + arr);
+console.log("длина массива:" + arr.length);
+
+var matrix = [
+    [5, 15, 76], [25, "число", 5], [20, 51,"буква"]
+]
+console.log(matrix)
